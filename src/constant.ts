9bd567f8c6017d41975dfe0696c8ac5303bdac07
Hayan Khan <hayankhanmehsood@gmail.com>
2024-11-03 15:43:46 +0500

    .,,

---
+++ src/constant.ts
@@ -0,0 +1,14 @@
+//array of navbar items
+export const NAVBAR_ITEMS = [
+    {
+        title: 'Home',
+        href: '/'
+    },
+    {
+        title: 'About',
+        href: '/about'
+    },
+    {
+        title: 'Contact',
+        href: '/contact'
+    }]
